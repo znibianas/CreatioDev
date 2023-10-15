7db4307f552adee1139bd9755a36c51c58dfe334
znibianas <znibianas.5555@gmail.com>
2023-10-15 12:01:13 +0100

    Realty FRUI Section

---
+++ UsrRealtyFRUI/Schemas/UsrRealtyFRUI_Detail/UsrRealtyFRUI_Detail.js
@@ -0,0 +1,8 @@
+define('UsrRealtyFRUI_Detail', [], function () {
+	return {
+		entitySchemaName: 'UsrRealtyFRUI',
+		details: /**SCHEMA_DETAILS*/ {} /**SCHEMA_DETAILS*/,
+		diff: /**SCHEMA_DIFF*/ [] /**SCHEMA_DIFF*/,
+		methods: {},
+	};
+});
---
+++ UsrRealtyFRUI/Schemas/UsrRealtyFRUI_FormPage/UsrRealtyFRUI_FormPage.js
@@ -0,0 +1,112 @@
+define("UsrRealtyFRUI_FormPage", /**SCHEMA_DEPS*/[]/**SCHEMA_DEPS*/, function/**SCHEMA_ARGS*/()/**SCHEMA_ARGS*/ {
+	return {
+		viewConfigDiff: /**SCHEMA_VIEW_CONFIG_DIFF*/[
+			{
+				"operation": "insert",
+				"name": "UsrName",
+				"values": {
+					"layoutConfig": {
+						"column": 1,
+						"row": 1,
+						"colSpan": 1,
+						"rowSpan": 1
+					},
+					"type": "crt.Input",
+					"label": "$Resources.Strings.UsrName",
+					"control": "$UsrName",
+					"labelPosition": "auto"
+				},
+				"parentName": "SideAreaProfileContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "merge",
+				"name": "AttachmentList",
+				"values": {
+					"type": "crt.FileList",
+					"masterRecordColumnValue": "$Id",
+					"recordColumnName": "RecordId",
+					"layoutConfig": {
+						"colSpan": 2,
+						"column": 1,
+						"row": 1,
+						"rowSpan": 6
+					},
+					"items": "$AttachmentList",
+					"primaryColumnName": "AttachmentListDS_Id",
+					"columns": [
+						{
+							"id": "32d2a10e-26ca-4b99-91a7-f7a88f2381d3",
+							"code": "AttachmentListDS_Name",
+							"caption": "#ResourceString(AttachmentListDS_Name)#",
+							"dataValueType": 28,
+							"width": 200
+						}
+					],
+					"viewType": "gallery",
+					"tileSize": "small"
+				},
+				"parentName": "AttachmentsTabContainer",
+				"propertyName": "items",
+				"index": 0
+			},
+			{
+				"operation": "merge",
+				"name": "Feed",
+				"values": {
+					"type": "crt.Feed",
+					"feedType": "Record",
+					"primaryColumnValue": "$Id",
+					"cardState": "$CardState",
+					"dataSourceName": "PDS",
+					"entitySchemaName": "UsrRealtyFRUI"
+				},
+				"parentName": "FeedTabContainer",
+				"propertyName": "items",
+				"index": 0
+			}
+		]/**SCHEMA_VIEW_CONFIG_DIFF*/,
+		viewModelConfig: /**SCHEMA_VIEW_MODEL_CONFIG*/{
+			"attributes": {
+				"UsrName": {
+					"modelConfig": {
+						"path": "PDS.UsrName"
+					}
+				},
+				"Id": {
+					"modelConfig": {
+						"path": "PDS.Id"
+					}
+				}
+			}
+		}/**SCHEMA_VIEW_MODEL_CONFIG*/,
+		modelConfig: /**SCHEMA_MODEL_CONFIG*/{
+			"dataSources": {
+				"PDS": {
+					"type": "crt.EntityDataSource",
+					"config": {
+						"entitySchemaName": "UsrRealtyFRUI"
+					},
+					"scope": "page"
+				},
+				"AttachmentListDS": {
+					"type": "crt.EntityDataSource",
+					"scope": "viewElement",
+					"config": {
+						"entitySchemaName": "SysFile",
+						"attributes": {
+							"Name": {
+								"path": "Name"
+							}
+						}
+					}
+				}
+			},
+			"primaryDataSourceName": "PDS"
+		}/**SCHEMA_MODEL_CONFIG*/,
+		handlers: /**SCHEMA_HANDLERS*/[]/**SCHEMA_HANDLERS*/,
+		converters: /**SCHEMA_CONVERTERS*/{}/**SCHEMA_CONVERTERS*/,
+		validators: /**SCHEMA_VALIDATORS*/{}/**SCHEMA_VALIDATORS*/
+	};
+});
